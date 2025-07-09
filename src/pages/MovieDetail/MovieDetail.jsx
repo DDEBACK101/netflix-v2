4617a7985b8c0ae19ca1d7a7aa96463da0cb0cec
송승현 <thdrlwjd1111@gmail.com>
2025-07-09 16:43:30 +0900

    ✅ 프로젝트 최종 수정: 반응형, detail page 등 적용 완료

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,29 +1,61 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Badge, Button, Alert, Modal } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Badge,
+  Button,
+  Alert,
+  Modal,
+} from "react-bootstrap";
 import { useMovieDetailQuery } from "../../hooks/useMovieDetail";
 import { useMovieReviewQuery } from "../../hooks/useMovieReview";
 import { useMovieRecommendation } from "../../hooks/useMovieRecommendation";
 import { useMovieVideoQuery } from "../../hooks/useMovieVideo";
 import { ClipLoader } from "react-spinners";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import MovieCard from "../../common/MovieCard/MovieCard";
 import YouTube from "react-youtube";
 import "./MovieDetail.style.css";
 
 const MovieDetail = () => {
   const { movie_id } = useParams();
+  const navigate = useNavigate();
+
   const { data, isLoading, isError, error } = useMovieDetailQuery(movie_id);
+  const {
+    data: reviewData,
+    isLoading: isReviewLoading,
+    isError: isReviewError,
+    error: reviewError,
+  } = useMovieReviewQuery(movie_id);
+  const {
+    data: recommendationData,
+    isLoading: isRecommendationLoading,
+    isError: isRecommendationError,
+    error: recommendationError,
+  } = useMovieRecommendation(movie_id);
+  const { data: videoData } = useMovieVideoQuery(movie_id);
 
   const [showReviews, setShowReviews] = useState(false);
   const [showRecommendations, setShowRecommendations] = useState(false);
   const [showTrailer, setShowTrailer] = useState(false);
-  const { data: reviewData, isLoading: isReviewLoading, isError: isReviewError, error: reviewError } = useMovieReviewQuery(movie_id);
-  const { data: recommendationData, isLoading: isRecommendationLoading, isError: isRecommendationError, error: recommendationError } = useMovieRecommendation(movie_id);
-  const { data: videoData } = useMovieVideoQuery(movie_id);
+
+  const handleWatchTrailer = () => setShowTrailer(true);
+  const handleCloseTrailer = () => setShowTrailer(false);
+  const handleShowReviews = () => setShowReviews(!showReviews);
+  const handleShowRecommendation = () =>
+    setShowRecommendations(!showRecommendations);
+
+  const trailerKey = videoData?.find(
+    (video) => video.type === "Trailer" && video.site === "YouTube"
+  )?.key;
 
   console.log("detail", data); // 콘솔에 데이터 출력
   console.log("movieid", movie_id);
+  console.log("추천데이터", recommendationData);
 
   if (isLoading) {
     return (
@@ -44,26 +76,8 @@ const MovieDetail = () => {
     return <Alert variant="danger">{error.message}</Alert>;
   }
 
-  const handleWatchTrailer = () => {
-    setShowTrailer(true);
-  };
-
-  const handleCloseTrailer = () => {
-    setShowTrailer(false);
-  };
-
-  const handleShowReviews = () => {
-    setShowReviews(!showReviews);
-  };
-
-  const handleShowRecommendation = () => {
-    setShowRecommendations(!showRecommendations);
-  };
-
-  const trailerKey = videoData?.find((video) => video.type === "Trailer" && video.site === "YouTube")?.key;
-
   return (
-    <Container>
+    <Container style={{ marginTop: "50px" }}>
       <Row>
         <Col>
           {data?.poster_path && (
@@ -77,16 +91,15 @@ const MovieDetail = () => {
         <Col>
           <h1>{data?.title}</h1>
           <div>
-            {data?.genres &&
-              data.genres.map((genre, index) => (
-                <Badge
-                  className={`genre_badge ${index === 0 ? "first_genre" : ""}`}
-                  key={genre.id}
-                  bg="danger"
-                >
-                  {genre.name}
-                </Badge>
-              ))}
+            {data?.genres?.map((genre, index) => (
+              <Badge
+                className={`genre_badge ${index === 0 ? "first_genre" : ""}`}
+                key={genre.id}
+                bg="danger"
+              >
+                {genre.name}
+              </Badge>
+            ))}
           </div>
           <hr />
           <h4>
@@ -110,26 +123,26 @@ const MovieDetail = () => {
           <hr />
           <div className="budget">
             <Badge pill bg="danger">
-              budget{" "}
-            </Badge>
+              budget
+            </Badge>{" "}
             &nbsp;: {data?.budget}
           </div>
           <div className="revenue">
             <Badge pill bg="danger">
-              revenue{" "}
-            </Badge>
+              revenue
+            </Badge>{" "}
             &nbsp;: {data?.revenue}
           </div>
           <div className="release_date">
             <Badge pill bg="danger">
-              release_date{" "}
-            </Badge>
+              release_date
+            </Badge>{" "}
             &nbsp;: {data?.release_date}
           </div>
           <div className="time">
             <Badge pill bg="danger">
-              time{" "}
-            </Badge>
+              time
+            </Badge>{" "}
             &nbsp;: {data?.runtime} minutes
           </div>
           <hr />
@@ -150,97 +163,86 @@ const MovieDetail = () => {
           </div>
         </Col>
       </Row>
+
       <Row>
         <Col>
-          {showReviews && (
-            <>
-              {isReviewLoading ? (
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                  }}
-                >
-                  <ClipLoader color={"#123abc"} loading={isReviewLoading} size={150} />
-                </div>
-              ) : isReviewError ? (
-                <Alert variant="danger">{reviewError.message}</Alert>
-              ) : (
-                <div className="reviews_container">
-                  <h2 className="reviews_header">Reviews:</h2>
-                  {reviewData && reviewData.length > 0 ? (
-                    <ul>
-                      {reviewData.map((review, index) => (
-                        <li className="review_content" key={index}>
-                          <h2>author - {review.author}</h2>
-                          <p>{review.content}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <p>Reviews do not exist.</p>
-                  )}
-                </div>
-              )}
-            </>
-          )}
+          {showReviews &&
+            (isReviewLoading ? (
+              <div>
+                <ClipLoader
+                  color={"#123abc"}
+                  loading={isReviewLoading}
+                  size={150}
+                />
+              </div>
+            ) : isReviewError ? (
+              <Alert variant="danger">{reviewError.message}</Alert>
+            ) : (
+              <div className="reviews_container">
+                <h2 className="reviews_header">Reviews:</h2>
+                {reviewData?.length > 0 ? (
+                  <ul>
+                    {reviewData.map((review, index) => (
+                      <li className="review_content" key={index}>
+                        <h2 className="review-author">
+                          author - {review.author}
+                        </h2>
+                        <p className="review-content">{review.content}</p>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>Reviews do not exist.</p>
+                )}
+              </div>
+            ))}
         </Col>
+
         <Col>
-          {showRecommendations && (
-            <>
-              {isRecommendationLoading ? (
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                  }}
-                >
-                  <ClipLoader color={"#123abc"} loading={isRecommendationLoading} size={150} />
-                </div>
-              ) : isRecommendationError ? (
-                <Alert variant="danger">{recommendationError.message}</Alert>
-              ) : (
-                <div className="recommendation_container">
-                  <h2 className="recommendation_header">Related Movies:</h2>
-                  {recommendationData && recommendationData.length > 0 ? (
-                    <Row className="related_move">
-                      {recommendationData.map((movie, index) => (
-                        <Col key={index} md={3} className="related_movie_item">
-                          {movie.poster_path && (
-                            <img
-                              src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
-                              alt={movie.title}
-                              className="related_movie_image"
-                            />
-                          )}
-                          <div>
-                            <h1 className="recommendation-title">{movie.title}</h1>                          
-                          </div>
-                        </Col>
-                      ))}
-                    </Row>
-                  ) : (
-                    <p>No related movies found.</p>
-                  )}
-                </div>
-              )}
-            </>
-          )}
+          {showRecommendations &&
+            (isRecommendationLoading ? (
+              <div>
+                <ClipLoader
+                  color={"#123abc"}
+                  loading={isRecommendationLoading}
+                  size={150}
+                />
+              </div>
+            ) : isRecommendationError ? (
+              <Alert variant="danger">{recommendationError.message}</Alert>
+            ) : (
+              <div className="recommendation_container">
+                <h2 className="recommendation_header">Related Movies:</h2>
+                {recommendationData?.length > 0 ? (
+                  <Row className="related_move">
+                    {recommendationData.map((movie, index) => (
+                      <Col key={index} md={3} xs={6}>
+                        <MovieCard
+                          className="move-card"
+                          movie={movie}
+                          onClick={() => navigate(`/movies/${movie.id}`)}
+                        />
+                      </Col>
+                    ))}
+                  </Row>
+                ) : (
+                  <p>No related movies found.</p>
+                )}
+              </div>
+            ))}
         </Col>
-      </Row> 
-      
-      {/* Modal for YouTube */}
+      </Row>
+
       <Modal show={showTrailer} onHide={handleCloseTrailer} centered>
         <Modal.Header closeButton>
           <Modal.Title>Watch Trailer</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {trailerKey ? (
-            <YouTube videoId={trailerKey} opts={{ width: '100%', height: '390px' }} />
+            <YouTube
+              videoId={trailerKey}
+              opts={{ width: "100%", height: "390px" }}
+            />
           ) : (
             <p>No trailer available.</p>
           )}
